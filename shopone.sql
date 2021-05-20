1a57156f64f3dfade8465d5c0acc810b0d9e6c10
Jamelich <serj-post@mail.ru>
2021-05-20 21:09:02 +0300

    Обновлена БД

--- shopone.sql
+++ shopone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 17 2021 г., 14:31
+-- Время создания: Май 20 2021 г., 21:06
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.1.33
 
@@ -31,8 +31,40 @@ CREATE TABLE `products` (
   `id` int NOT NULL,
   `prod_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `prod_desc` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `price` int NOT NULL
+  `price` int NOT NULL,
+  `prod_img` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `prod_name`, `prod_desc`, `price`, `prod_img`) VALUES
+(1, 'Ноутбук Acer', 'Описание', 100, '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `login` varchar(56) NOT NULL,
+  `password` varchar(56) NOT NULL,
+  `full_name` varchar(128) NOT NULL,
+  `status` int NOT NULL,
+  `e-mail` varchar(56) NOT NULL,
+  `phone` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `addres` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `full_name`, `status`, `e-mail`, `phone`, `addres`) VALUES
+(1, 'admin', 'admin', 'Administrator', 1, 'serj-post@mail.ru', '89621415543', 'Адрес');
 
 --
 -- Индексы сохранённых таблиц
@@ -45,6 +77,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -52,7 +90,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
